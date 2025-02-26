bd5c38f84b8d3661ec5a41a9b69ddf826c8154f6
Lennin Rodriguez <lennin.rodriguez1997@gmail.com>
2025-02-26 12:49:38 -0800

    adding new settings for Digital Ocean

--- djangobackend/backend/settings.py
+++ djangobackend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#g=pt5g@)wvj2j!936lue-!&p5@p6bv(!ac30#!clryq51l%nx'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
